a8624f3433d81acc7d7277949dd03b6ef11ef027
Daniel Leal <daniel.leal@auto1.com>
2019-01-31 17:22:13 +0100

    Add screenshots for ViewHolder

--- app/src/androidTest/java/com/karumi/ui/view/SuperHeroViewHolderTest.kt
+++ app/src/androidTest/java/com/karumi/ui/view/SuperHeroViewHolderTest.kt
@@ -6,6 +6,8 @@ import com.karumi.R
 import com.karumi.domain.model.SuperHero
 import com.karumi.ui.presenter.SuperHeroesPresenter
 import com.karumi.ui.view.adapter.SuperHeroViewHolder
+import com.karumi.utils.TextLength
+import com.karumi.utils.getTestTextByLength
 import org.junit.Test
 import org.mockito.Mockito.mock
 
@@ -22,8 +24,28 @@ class SuperHeroViewHolderTest : ScreenshotTest {
     }
 
     @Test
-    fun showsSuperHeroWithLongDescription() {
-        val superHero = givenASuperHeroWithALongDescription()
+    fun showsAvenger() {
+        val superHero = givenASuperHero(isAvenger = true)
+        val holder = givenASuperHeroViewHolder()
+
+        holder.render(superHero)
+
+        compareScreenshot(holder, R.dimen.super_hero_row_height)
+    }
+
+    @Test
+    fun showsSuperHeroWithNoName() {
+        val superHero = givenASuperHeroWithNameLength(TextLength.EMPTY)
+        val holder = givenASuperHeroViewHolder()
+
+        holder.render(superHero)
+
+        compareScreenshot(holder, R.dimen.super_hero_row_height)
+    }
+
+    @Test
+    fun showsSuperHeroWithShortName() {
+        val superHero = givenASuperHeroWithNameLength(TextLength.SHORT)
         val holder = givenASuperHeroViewHolder()
 
         holder.render(superHero)
@@ -33,7 +55,17 @@ class SuperHeroViewHolderTest : ScreenshotTest {
 
     @Test
     fun showsSuperHeroWithLongName() {
-        val superHero = givenASuperHeroWithALongName()
+        val superHero = givenASuperHeroWithNameLength(TextLength.LONG)
+        val holder = givenASuperHeroViewHolder()
+
+        holder.render(superHero)
+
+        compareScreenshot(holder, R.dimen.super_hero_row_height)
+    }
+
+    @Test
+    fun showsSuperHeroWithVeryLongName() {
+        val superHero = givenASuperHeroWithNameLength(TextLength.VERY_LONG)
         val holder = givenASuperHeroViewHolder()
 
         holder.render(superHero)
@@ -46,20 +78,14 @@ class SuperHeroViewHolderTest : ScreenshotTest {
         val inflater = LayoutInflater.from(context)
         val view = inflater.inflate(R.layout.super_hero_row, null, false)
         return SuperHeroViewHolder(
-            view,
-            mock<SuperHeroesPresenter>(SuperHeroesPresenter::class.java)
+                view,
+                mock<SuperHeroesPresenter>(SuperHeroesPresenter::class.java)
         )
     }
 
-    private fun givenASuperHeroWithALongDescription(): SuperHero {
-        val superHeroName = "Super Hero Name"
-        val superHeroDescription = """
-            |Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            |incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-            |ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-            |voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            |proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            |""".trimMargin()
+    private fun givenASuperHeroWithNameLength(nameLength: TextLength): SuperHero {
+        val superHeroName = getTestTextByLength(nameLength)
+        val superHeroDescription = "Description Super Hero"
         val isAvenger = false
         return givenASuperHero(superHeroName, superHeroDescription, isAvenger)
     }
@@ -78,8 +104,8 @@ class SuperHeroViewHolderTest : ScreenshotTest {
     }
 
     private fun givenASuperHero(
-        superHeroName: String = "Super Hero Name",
-        superHeroDescription: String = "Super Hero Description",
-        isAvenger: Boolean = false
+            superHeroName: String = "Super Hero Name",
+            superHeroDescription: String = "Super Hero Description",
+            isAvenger: Boolean = false
     ): SuperHero = SuperHero(superHeroName, null, isAvenger, superHeroDescription)
 }
